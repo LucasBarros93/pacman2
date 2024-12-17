c1b8558364f5d7f5c476483b330505f291d0e31e
NinjaPro93 <ninjapro93lu@gmail.com>
2024-12-17 02:27:46 -0300

    score comentado

--- src/score.cpp
+++ src/score.cpp
@@ -1,17 +1,20 @@
 #include "score.hpp"
 
+// Construtor da classe Score, inicializa o valor da pontuação (valor padrão = 0)
 Score::Score(int val = 0) : value(val) {}
 
-
+// Sobrecarga do operador "+" para somar pontos
 Score Score::operator+(int points) const {
-    return Score(value + points);
+    return Score(value + points); // Cria um novo objeto com o valor somado
 }
 
+// Sobrecarga do operador "+=" para adicionar pontos ao valor atual
 Score& Score::operator+=(int points) {
-    value += points;
-    return *this;
+    value += points; // Adiciona os pontos ao valor atual
+    return *this;    // Retorna o objeto modificado
 }
 
+// Método para acessar o valor da pontuação
 int Score::getValue() const {
-    return value;
+    return value; // Retorna o valor armazenado no objeto
 }
