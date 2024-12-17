2d4bd92431a2248983eeafbef81139f0b6522a6f
AndreBorba <andrevillalba2.av@gmail.com>
2024-12-17 02:59:58 -0300

    comentarios pacman.hpp

--- inc/entitys/pacman.hpp
+++ inc/entitys/pacman.hpp
@@ -5,38 +5,37 @@
 #include <algorithm>
 #include <SFML/Graphics.hpp>
 
-typedef std::vector<std::vector <char>> MapData;
+typedef std::vector<std::vector <char>> MapData; // Tipo de dado para o mapa (representado por uma matriz de caracteres)
 
-class Pacman {
+// ============================
+// Classe Pacman
+// ============================
+// Classe do personagem principal (PacMan)
+// Lida com a animacao, posicao, direcao e interacao com o mapa
+class Pacman{
     private:
-        sf::Sprite sprite;       // Sprite para representar o Pacman visualmente
-        sf::Texture texture;     // Textura para carregar a imagem do Pacman
-        sf::Vector2<int> pos;        // Posição atual (x, y)
-        sf::Vector2<int> dir;        // Direção atual (-1, 0, ou 1 para x e y)
-
-        sf::IntRect currentFrame;
-        sf::Clock animationClock;
-
-        const int frameWidth;      // Largura do frame (ex: 16 pixels)
-        const int frameHeight;     // Altura do frame (ex: 16 pixels)
-        const int frameCount;      // Quantidade de frames por direção
-        float frameDuration;       // Tempo entre frames
-        int currentFrameIndex;     // Índice do frame atual
+        sf::Sprite sprite;          // Sprite pra representar o Pacman visualmente
+        sf::Texture texture;        // Textura pra carregar a imagem do Pacman
+        sf::Vector2<int> pos;       // Posicao atual (x, y)
+        sf::Vector2<int> dir;       // Direcao atual (-1, 0, ou 1 para x e y)
+        sf::IntRect currentFrame;   // Retangulo que seleciona o frame atual do png
+        sf::Clock animationClock;   // Relogio que  controla o tempo entre frames
+        const int frameWidth;       // Largura do frame (ex: 16 pixels)
+        const int frameHeight;      // Altura do frame (ex: 16 pixels)
+        const int frameCount;       // Quantidade de frames por direcao
+        float frameDuration;        // Tempo entre frames
+        int currentFrameIndex;      // Indice do frame atual
 
     public:
-        Pacman(const std::string& texturePath, int fw, int fh, float fd);
-
-        void updateAnimation();
-        MapData update(MapData mapData, const sf::Vector2<int> direction);
+        Pacman(const std::string& texturePath, int fw, int fh, float fd); // Construtor: Inicia pacman com uma textura, dimensoes e duracao do frame
+        void updateAnimation(); // Atualiza a animacao do pacman (troca os frames)
+        MapData update(MapData mapData, const sf::Vector2<int> direction); // Atualiza a posicao do pacman no mapa com base na direcao
         void reset(); // reseta dps do gameover
-
-
-        void setDirection(const sf::Vector2<int>& direction); // Define a direção de movimento
-        void setPosition(const sf::Vector2<int>& position, const sf::Vector2<float>& tileSize);
-
-        const sf::Sprite getSprite() const;             // Desenha o Pacman na janela
-        const sf::Vector2<int>& getPosition() const;         // Retorna a posição atual
-        const sf::Vector2<int>& getDirection() const;        // Retorna a direção atual
+        void setDirection(const sf::Vector2<int>& direction); // Define a direcao de movimento do pac
+        void setPosition(const sf::Vector2<int>& position, const sf::Vector2<float>& tileSize); // Define o lugar inicial do pacman na tela
+        const sf::Sprite getSprite() const; // Desenha o Pacman na janela
+        const sf::Vector2<int>& getPosition() const; // Retorna a posicao atual dele
+        const sf::Vector2<int>& getDirection() const; // Retorna a direcao atual que se movimenta
 };
 
 #endif // PACMAN_HPP
