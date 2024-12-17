fbc5bfb9c5ab3f90e84db31826578706e3ae676c
AndreBorba <andrevillalba2.av@gmail.com>
2024-12-17 13:31:54 -0300

    comentando ainda fruits.cpp

--- src/entitys/fruits.cpp
+++ src/entitys/fruits.cpp
@@ -2,7 +2,7 @@
 
 // -----------------------------------
 // Função auxiliar para números aleatórios
-int getRandomNumber(int min, int max) {
+int getRandomNumber(int min, int max){
     static std::random_device rd;
     static std::mt19937 mt(rd());
     std::uniform_int_distribution<int> dist(min, max);
@@ -10,28 +10,32 @@ int getRandomNumber(int min, int max) {
 }
 
 // ===================================
-// Implementação da classe Dot
-Dot::Dot(sf::Vector2<int> position) : pos(position), points(10) {}
+// IMplementacao da classe Dot
+Dot::Dot(sf::Vector2<int> position) : pos(position), points(10) {} // Construtor que inicializa posicao e pontos da fruta
 
-int Dot::getPoints() const {
+// Aqui retorna os pontos do ponto (dot)
+int Dot::getPoints() const{
     return this->points;
 }
 
-void Dot::draw(sf::RenderWindow& window, const sf::Vector2f& offset) const {
+// Metodo pra desenhar o Dot na janela, uma bolinha. Offset foi aplicado pra centralizar o Dot na tela
+void Dot::draw(sf::RenderWindow& window, const sf::Vector2f& offset) const{
     sf::CircleShape dot(2.0f);
     dot.setFillColor(sf::Color(184, 197, 201));
-    dot.setPosition(((this->pos.x * 10) + 10) + offset.x, ((this->pos.y * 10 )+10) + offset.y);
+    dot.setPosition(((this->pos.x * 10) + 10) + offset.x, ((this->pos.y * 10 )+10) + offset.y); // Aqui a aplicacao do offset, tem isso em todos os elementos graficos pra eles se alinharem no meio da tela
     window.draw(dot);
 }
 
 // ===================================
-// Implementação da classe Energizer
-Energizer::Energizer(sf::Vector2<int> position) : pos(position), points(50) {}
+// Implementacao da classe Energizer
+Energizer::Energizer(sf::Vector2<int> position) : pos(position), points(50) {} // Construtor que inicializa posicao e pontos da fruta
 
+// Metodo de retornar os pontos do energizer
 int Energizer::getPoints() const {
     return this->points;
 }
 
+// Metodo pra desenhar o energizer como um ponto maior, bolinha branca maior que o Dot
 void Energizer::draw(sf::RenderWindow& window, const sf::Vector2f& offset) const {
     sf::CircleShape energizer(4.0f);
     energizer.setFillColor(sf::Color(184, 197, 201));
@@ -40,80 +44,80 @@ void Energizer::draw(sf::RenderWindow& window, const sf::Vector2f& offset) const
 }
 
 // ===================================
-// Implementação da classe Bonus
+// Implementacao da classe Bonus
 Bonus::Bonus(int fw, int fh) 
-    : frameWidth(fw), frameHeight(fh), active(false) {
+    : frameWidth(fw), frameHeight(fh), active(false){
     this->frame = sf::IntRect(0, 0, this->frameWidth, this->frameHeight);
     this->points = 0; // Inicializa pontos
-}
+} // Construtor que inicializa o tamanho so sprite e tambem define a fruta bonus como inativa
 
+// Metodo q procura uma posicao livre no mapa pra poder colocar a fruta bonus aleatoriamente
 void Bonus::spawn(const std::vector<std::vector<char>>& mapData) {
-    // Verifica se a fruta bônus já está ativa
-    if (this->active) return;
+    // Verifica se a fruta bonus ja esta ativa
+    if(this->active){
+      return;  
+    } 
 
-    // Encontra posições livres no mapa
+    // Encontra posicoes livres no mapa
     std::vector<sf::Vector2<int>> emptyPositions;
-    for (size_t y = 0; y < mapData.size(); y+=2) {
-        for (size_t x = 0; x < mapData[y].size(); x+=2) {
-            if (mapData[y][x] == ' ') {
+    for(size_t y = 0; y < mapData.size(); y+=2){
+        for(size_t x = 0; x < mapData[y].size(); x+=2){
+            if(mapData[y][x] == ' ') {
                 emptyPositions.push_back({static_cast<int>(x), static_cast<int>(y)});
             }
         }
     }
 
-    if (!emptyPositions.empty()) {
-        // Gera uma posição aleatória
+    if(!emptyPositions.empty()){
+        // gera uma posicao aleatoria dentre aquelas q estao disponiveis
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_int_distribution<> dist(0, emptyPositions.size() - 1);
         this->pos = emptyPositions[dist(gen)];
 
-        // Aleatoriza pontos e textura
+        // Deixa a textura e os pontos da frta aleatorios
         randomize();
 
-        // Ativa a fruta bônus
-        this->active = true;
-        this->visibilityTimer.restart(); // Reinicia o tempo de visibilidade
+        this->active = true; // Ativa a fruta bonus
+        this->visibilityTimer.restart(); // Reinicia o tempo de visibilidade (a fruta bonus so fica ativa durante 10seg)
     }
 }
 
-void Bonus::update(const std::vector<std::vector<char>>& mapData) {
-    if (active) {
-        // Se a fruta estiver visível por mais de 10 segundos, desativa
-        if (visibilityTimer.getElapsedTime().asSeconds() > 10.0f) {
+// Metodo pra atualizar o status da fruta bonus ---> ativa por 10 segundos e desativa por 15 segundos
+void Bonus::update(const std::vector<std::vector<char>>& mapData){
+    if(active){
+        // Se a fruta estiver visivel por mais de 10seg ---> desativa fruta
+        if(visibilityTimer.getElapsedTime().asSeconds() > 10.0f){
             active = false;
             respawnTimer.restart(); // Inicia o temporizador de respawn
         }
-    } else {
-        // Se a fruta estiver inativa por 15 segundos, reaparece
-        if (respawnTimer.getElapsedTime().asSeconds() > 15.0f) {
+    } 
+    else{
+        // Se a fruta estiver inativa por 15seg ---> reaparece a fruta
+        if(respawnTimer.getElapsedTime().asSeconds() > 15.0f){
             spawn(mapData); // Passa o mapa como argumento
         }
     }
 }
 
 
-
+// Metodo pra deixar o peso e a textura da fruta aleatorios
 void Bonus::randomize() {
-    const int pointsOptions[8] = {100, 300, 500, 700, 1000, 2000, 3000, 5000};
-    std::vector<int> weights = {40, 20, 15, 10, 7, 5, 2, 1}; 
+    const int pointsOptions[8] = {100, 300, 500, 700, 1000, 2000, 3000, 5000}; // Possibilidades de pontos que a frut pode dar pro pacman caso ele a pegue
+    std::vector<int> weights = {40, 20, 15, 10, 7, 5, 2, 1}; // peso pra cada valor possivel de pontuacao (maior peso -> maior probabilidade)
 
-
-    // Configuração do gerador de números aleatórios
-    std::random_device rd;  // Fonte de entropia
-    std::mt19937 gen(rd()); // Gerador de números aleatórios
+    // Gera um indice aleatorio com base nos pesos
+    std::random_device rd;  
+    std::mt19937 gen(rd()); 
     std::discrete_distribution<> dist(weights.begin(), weights.end());
     int textureIndex = dist(gen);
 
+    // Configura a textura pegando uma imagem da 'base de dados' de imagens
     std::string texturePath = "./assets/images/spritesheet.png";
-    if (!texture.loadFromFile(texturePath)) {
-        std::cerr << "Erro ao carregar textura: " << texturePath << std::endl;
-    }
-
+    texture.loadFromFile(texturePath);
     int offsetX = (textureIndex * 16) + 32;
     this->frame.left = offsetX;
     this->frame.top = 48;
-
     this->sprite.setTexture(texture);
     this->sprite.setTextureRect(this->frame);
     this->points = pointsOptions[textureIndex];
@@ -124,7 +128,7 @@ int Bonus::getPoints() const {
 }
 
 void Bonus::draw(sf::RenderWindow& window, const sf::Vector2f& offset) const {
-    if (this->active) {
+    if(this->active) {
         sf::Sprite spriteCopy = this->sprite;
         spriteCopy.setPosition((this->pos.x * 10) + offset.x, (this->pos.y * 10) + offset.y);
         window.draw(spriteCopy);
