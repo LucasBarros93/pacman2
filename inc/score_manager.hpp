a3fd5d8a88303d001558a8e50b2112f45e7cc5fe
AndreBorba <andrevillalba2.av@gmail.com>
2024-12-17 03:33:28 -0300

    score_manager.hpp comentado

--- inc/score_manager.hpp
+++ inc/score_manager.hpp
@@ -8,24 +8,32 @@
 #include <sstream>
 #include <iostream>
 
-struct ScoreEntry {
-    std::string name;
-    int score;
 
-    ScoreEntry(const std::string& name, int score) : name(name), score(score) {}
+// Esse arquivo eh diferente do "score.hpp" pq ele trata do score armazenado no quadro de lideres, enquanto o outro lida com o score atualizado "ao vivo"
+
+// Essa struct eh uma entrada individual no quadro de lideres
+struct ScoreEntry{
+    std::string name; // Nome do jogador
+    int score; // Pontuacao do jogador
+    ScoreEntry(const std::string& name, int score) : name(name), score(score) {} // Construtor q so inicia o nome e o score
 };
 
-
-class ScoreManager {
+// =====================================
+// Classe ScoreManager
+// =====================================
+// Lida com o sistema de quadro de lideres:
+// - Carrega pontos de um arquivo
+// - Salva novas pontuacoes
+class ScoreManager{
     private:
-        std::string filePath;
-        std::vector<std::pair<std::string, int>> scores;
-        void loadScores();
+        std::string filePath; // Caminho de onde ta o .txt com o top5
+        std::vector<std::pair<std::string, int>> scores; // Vetor pra armazenar as pontuacoes e os nomes
+        void loadScores(); // Carrega as pontuacoes e nomes do arquivo e depois escreve elas no vetor "scores"
     
     public:
         ScoreManager(const std::string& filePath);
-        void saveScore(const std::string& playerName, int score);
-        std::vector<std::pair<std::string, int>> getScores() const;
+        void saveScore(const std::string& playerName, int score); //Salva uma nova pontuacao no quadro de lideres
+        std::vector<std::pair<std::string, int>> getScores() const; // Retorna o vetor de pontuacoes e nomes 
 };
 
 #endif
